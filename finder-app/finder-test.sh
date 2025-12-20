af3aff7aa3c8a7b64bedf4a49750a8b0e778006b
FabSpit <fabian.s.spitz@protonmail.com>
2025-12-20 13:23:55 +0100

    changed finder-test.sh to be able to run it on embedded linux device

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Tester script for assignment 1 and assignment 2
+# Modified for assignment 4 Buildroot/QEMU requirements
 # Author: Siddhant Jajoo
 
 set -e
@@ -8,21 +9,23 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+
+# ÄNDERUNG: Pfad zur username.txt auf /etc/finder-app/conf/ angepasst
+username=$(cat /etc/finder-app/conf/username.txt)
 
 if [ $# -lt 3 ]
 then
-	echo "Using default value ${WRITESTR} for string to write"
-	if [ $# -lt 1 ]
-	then
-		echo "Using default value ${NUMFILES} for number of files to write"
-	else
-		NUMFILES=$1
-	fi	
+    echo "Using default value ${WRITESTR} for string to write"
+    if [ $# -lt 1 ]
+    then
+        echo "Using default value ${NUMFILES} for number of files to write"
+    else
+        NUMFILES=$1
+    fi  
 else
-	NUMFILES=$1
-	WRITESTR=$2
-	WRITEDIR=/tmp/aeld-data/$3
+    NUMFILES=$1
+    WRITESTR=$2
+    WRITEDIR=/tmp/aeld-data/$3
 fi
 
 MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
@@ -31,33 +34,34 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 
 rm -rf "${WRITEDIR}"
 
-# create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+# ÄNDERUNG: Pfad zur assignment.txt auf /etc/finder-app/conf/ angepasst
+assignment=`cat /etc/finder-app/conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
-	mkdir -p "$WRITEDIR"
+    mkdir -p "$WRITEDIR"
 
-	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
-	#The quotes signify that the entire string in WRITEDIR is a single string.
-	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
-	if [ -d "$WRITEDIR" ]
-	then
-		echo "$WRITEDIR created"
-	else
-		exit 1
-	fi
+    if [ -d "$WRITEDIR" ]
+    then
+        echo "$WRITEDIR created"
+    else
+        exit 1
+    fi
 fi
-#echo "Removing the old writer utility and compiling as a native application"
-#make clean
-#make
+
+# Die Sektion für 'make' wurde entfernt, da Buildroot das Kompilieren übernimmt.
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+    # ÄNDERUNG: './' entfernt, da 'writer' im PATH (/usr/bin) liegt
+    writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+# ÄNDERUNG: './' entfernt, da 'finder.sh' im PATH (/usr/bin) liegt
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
+
+# ÄNDERUNG: Ausgabe in die spezifizierte Ergebnisdatei schreiben
+echo "${OUTPUTSTRING}" > /tmp/assignment4-result.txt
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
@@ -65,9 +69,9 @@ rm -rf /tmp/aeld-data
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
-	echo "success"
-	exit 0
+    echo "success"
+    exit 0
 else
-	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
-	exit 1
+    echo "failed: expected ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
+    exit 1
 fi
